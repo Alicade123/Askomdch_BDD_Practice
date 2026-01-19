3a24f702beafb1852cf74dd36955e045606bae4f
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-19 21:14:38 +0200

    Update Navigation step definitions

--- src/test/java/askomdch/stepdefinitions/navigation/NavigationStepDefinition.java
+++ src/test/java/askomdch/stepdefinitions/navigation/NavigationStepDefinition.java
@@ -1,38 +1,32 @@
 package askomdch.stepdefinitions.navigation;
 
 import askomdch.dependencyinjection.UtilClass;
+import askomdch.pages.HomePage;
 import askomdch.utils.DriverFactory;
+import askomdch.utils.WebsiteStateManager;
 import io.cucumber.java.en.*;
-import org.openqa.selenium.By;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
-import static org.junit.Assert.assertEquals;
 
 public class NavigationStepDefinition {
-    private UtilClass utilClass;
-    private WebDriver driver  = DriverFactory.getDriver();
-    private WebDriverWait wait;
-    public NavigationStepDefinition (UtilClass utilClass){
-        this.utilClass = utilClass;
-        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-    }
+    private WebDriver driver = DriverFactory.getDriver();
+    private final WebsiteStateManager websiteStateManager = new WebsiteStateManager(driver);
+    private final HomePage homePage = new HomePage(driver);
 
 
     @Given("I'm On any page of the AskOmDch website")
-    public void openSite(){
-        driver.get(UtilClass.SITEURL);
-        driver.manage().window().maximize();
+    public void openSite() {
+        websiteStateManager.loadPage(UtilClass.SITEURL);
     }
+
     @When("I click on {string}")
-    public void clickLink(String link){
-        wait.until(ExpectedConditions.elementToBeClickable(By.linkText(link))).click();
+    public void clickLink(String link) {
+        homePage.clickLink(link);
     }
+
     @Then("I should be directed to the page with {string}")
-    public void checkPage(String pageTitle){
-        assertEquals(pageTitle, driver.getTitle());
+    public void checkPage(String pageTitle) {
+        Assert.assertEquals(pageTitle, driver.getTitle());
     }
 }

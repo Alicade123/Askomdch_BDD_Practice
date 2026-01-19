4a6a9aa9f2581f544d25aa6ec6e2cc6f7bac95dc
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-19 21:15:37 +0200

    Add Cucumber test runner configuration

---
+++ src/test/java/askomdch/runner/TestRunnerClass.java
@@ -0,0 +1,19 @@
+package askomdch.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/askomdch/features",
+        glue = {"askomdch.stepdefinitions",
+                "askomdch.hooks",
+                "askomdch.dependencyinjection"
+                },
+        plugin = "pretty",
+        tags = "@addToCart and not @checkout and not @login and not @navigation and not @filterProductsByCategory and not @filterProductsByPriceRange and not @register"
+)
+public class TestRunnerClass {
+
+}

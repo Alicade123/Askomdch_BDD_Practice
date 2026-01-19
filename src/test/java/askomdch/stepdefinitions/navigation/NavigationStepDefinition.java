cefcf2f93ec58b27740565666725326c303c2ebf
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-19 08:59:49 +0200

    Add navigation step definitions

---
+++ src/test/java/askomdch/stepdefinitions/navigation/NavigationStepDefinition.java
@@ -0,0 +1,38 @@
+package askomdch.stepdefinitions.navigation;
+
+import askomdch.dependencyinjection.UtilClass;
+import askomdch.utils.DriverFactory;
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+
+public class NavigationStepDefinition {
+    private UtilClass utilClass;
+    private WebDriver driver  = DriverFactory.getDriver();
+    private WebDriverWait wait;
+    public NavigationStepDefinition (UtilClass utilClass){
+        this.utilClass = utilClass;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    }
+
+
+    @Given("I'm On any page of the AskOmDch website")
+    public void openSite(){
+        driver.get(UtilClass.SITEURL);
+        driver.manage().window().maximize();
+    }
+    @When("I click on {string}")
+    public void clickLink(String link){
+        wait.until(ExpectedConditions.elementToBeClickable(By.linkText(link))).click();
+    }
+    @Then("I should be directed to the page with {string}")
+    public void checkPage(String pageTitle){
+        assertEquals(pageTitle, driver.getTitle());
+    }
+}

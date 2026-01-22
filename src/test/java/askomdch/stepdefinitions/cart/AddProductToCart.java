88e85102c647dd61423cad6ca485bb99b57fd4d7
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-22 12:24:16 +0200

    Improve add product to cart step definitions

--- src/test/java/askomdch/stepdefinitions/cart/AddProductToCart.java
+++ src/test/java/askomdch/stepdefinitions/cart/AddProductToCart.java
@@ -1,9 +1,8 @@
 package askomdch.stepdefinitions.cart;
 
+import askomdch.domainobject.Product;
 import askomdch.pages.CartPage;
-import askomdch.pages.StorePage;
 import askomdch.utils.DriverFactory;
-import com.sun.source.tree.AssertTree;
 import io.cucumber.java.en.*;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
@@ -12,9 +11,9 @@ public class AddProductToCart {
     private final WebDriver driver = DriverFactory.getDriver();
     private final CartPage cartPage = new CartPage(driver);
 
-    @Then("On Cart Page I should see {int} {string} in the cart")
-    public void checkProductInCart(int productQuantity, String productName){
-        Assert.assertEquals("Not much", productName,cartPage.getProductName());
+    @Then("On Cart Page I should see {int} {product} in the cart")
+    public void checkProductInCart(int productQuantity, Product product){
+        Assert.assertEquals("Not much", product.getName(),cartPage.getProductName());
         Assert.assertEquals("Not much", productQuantity, cartPage.getProductQuantity());
     }
 }
